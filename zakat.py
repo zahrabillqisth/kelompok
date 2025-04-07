c64e3298f3aa70a4d5b7175c02485ace86777a1f
Nurulizzah-mail <nurulizzah098@gmail.com>
2025-04-07 11:12:43 +0700

    Tambah data zakat

--- zakat.py
+++ zakat.py
@@ -9,3 +9,14 @@ def create_connection():
         password="",   # Password MySQL
         database="zakat" # Nama database
     )
+# Fungsi untuk menambahkan data zakat
+def add_zakat(nama, jenis_zakat, jumlah, tanggal):
+    conn = create_connection()
+    cursor = conn.cursor()
+    
+    query = "INSERT INTO zakat_data (nama, jenis_zakat, jumlah, tanggal) VALUES (%s, %s, %s, %s)"
+    cursor.execute(query, (nama, jenis_zakat, jumlah, tanggal))
+    
+    conn.commit()
+    cursor.close()
+    conn.close()
