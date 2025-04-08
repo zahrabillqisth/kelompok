bc02f8625f460740f4646cb388b043f3b87fd04c
Zahra Billqisth <Billqisth.zahra88@gmail.com>
2025-04-09 00:28:36 +0700

    expor exel

--- zakat.py
+++ zakat.py
@@ -16,3 +16,17 @@ def view_transaksi_zakat():
     
     cursor.close()
     conn.close()
+
+# Fungsi untuk mengekspor data zakat ke Excel
+def export_to_excel():
+    conn = create_connection()
+    query = "SELECT * FROM zakat_data"
+    
+    # Mengambil data dari database
+    zakat_data = pd.read_sql(query, conn)
+    
+    # Mengekspor data ke dalam file Excel
+    zakat_data.to_excel("data_zakat.xlsx", index=False)
+    
+    conn.close()
+    print("Data zakat berhasil diekspor ke dalam file 'data_zakat.xlsx'")
